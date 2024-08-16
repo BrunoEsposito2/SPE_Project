8b4393c6c2f908574cdf0bfe4534e7794ec72341
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-08-16 18:03:14 +0200

    fix: modify build.gradle spotless starting tasks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ spotless {
 
 // Configurazione Git Hooks
 gitHooks {
-    setHooks(mapOf("commit-msg" to "startSpotless"))
-    //setHooks(mapOf("commit-msg" to "conventionalCommits"))
+    setHooks(mapOf("pre-commit" to "startSpotless"))
+    setHooks(mapOf("commit-msg" to "conventionalCommits"))
     setHooksDirectory(layout.projectDirectory.dir("../.git/hooks"))
 }
 
